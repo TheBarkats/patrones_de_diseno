80d498b5677c30897d074e0a55070a0fe4a185b3
Giamcito <juan.giraldo03@uceva.edu.co>
2026-05-14 20:37:26 -0500

    Creo componente singleton

---
+++ src/app/patrones/singleton/singleton.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { SingletonService } from './singleton.service';
+
+@Component({
+  selector: 'app-singleton',
+  templateUrl: './singleton.component.html',
+  styleUrls: ['./singleton.component.css'],
+})
+export class SingletonComponent {
+  result: string = '';
+
+  constructor(private service: SingletonService) {}
+
+  testInstances(): void {
+    const result = this.service.testInstances();
+    this.result = JSON.stringify(result, null, 2);
+  }
+
+  demonstrate(): void {
+    const logs = this.service.demonstrateOperations();
+    this.result = logs.join('\n');
+  }
+}
